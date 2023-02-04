380e9e71a4fd98494418ea7024f359fcd6008f14
Trevor <trjohnson.1994@gmail.com>
2023-02-03 22:09:06 -0500

    Add extra comments.

--- src/week5Lab/OOP/SpacedLogger.java
+++ src/week5Lab/OOP/SpacedLogger.java
@@ -6,7 +6,7 @@ public class SpacedLogger implements Logger {
   public void log(String log) { //add space in between each character of string argument passed to it
     String spacedMessage = ""; // empty variable
     
-    for (int i=0; i<log.length(); i++) { //loop log.length to add log.CharAt(index) + space
+    for (int i=0; i<log.length(); i++) { //loop log.length to add log.CharAt(index) + " "
       spacedMessage += log.charAt(i) + " ";
     }
     System.out.println(spacedMessage.trim());     
@@ -22,19 +22,18 @@ public class SpacedLogger implements Logger {
     return;
   }
   
-  
   public String spaceString(String word) { //put " " between every character in a string
     String bufferString = word;
     String spacedWord = "";
     
     for (int i=0; i<bufferString.length(); i++) {
-      if (i == (bufferString.length())) {
-        return spacedWord;
+      if (i == bufferString.length()) { //make sure int i won't go out of index bounds for word max length
+        return spacedWord; //return from loop
       } else {
       spacedWord += bufferString.charAt(i) + " ";
       } }
     
-    return spacedWord.trim();    
+    return spacedWord.trim(); //return word with spaces, no space on end
   }
 
 }
